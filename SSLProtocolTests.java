84cf1e062725fd38a7476160b28b51b927ba3823
bigwindlee <bigwindlee@hotmail.com>
2016-04-10 00:41:36 +0800

    my first commit

---
+++ SSLProtocolTests.java
@@ -0,0 +1,46 @@
+import javax.net.ssl.*;
+
+
+public class SSLProtocolTests {
+    public static void main(String[] args) throws Exception {
+        System.out.println("java.version=" + System.getProperty("java.version"));
+        
+        System.out.println("\nPrint SSL info of the default :");
+        printSSL(SSLContext.getDefault());
+        
+        System.out.println("\nPrint SSL info of the TLSv1.2 :");
+        SSLContext context2 = SSLContext.getInstance("TLSv1.2");
+        context2.init(null, null, null);
+        printSSL(context2);
+
+    }
+    
+    public static void printSSL(SSLContext context) throws Exception {
+        SSLSocketFactory factory = (SSLSocketFactory)context.getSocketFactory();
+        SSLSocket socket = (SSLSocket)factory.createSocket();
+
+        String[] protocols = socket.getSupportedProtocols();
+
+        System.out.println("Supported Protocols: " + protocols.length);
+        for(int i = 0; i < protocols.length; i++)
+        {
+            System.out.println(" " + protocols[i]);
+        }
+
+        protocols = socket.getEnabledProtocols();
+
+        System.out.println("Enabled Protocols: " + protocols.length);
+        for(int i = 0; i < protocols.length; i++)
+        {
+            System.out.println(" " + protocols[i]);
+        }
+
+
+        String[] ciphers = socket.getSupportedCipherSuites();
+        System.out.println("Enabled Ciphers: " + ciphers.length);
+        for(int i = 0; i < ciphers.length; i++)
+        {
+            System.out.println(" " + ciphers[i]);
+        }
+    }
+}
