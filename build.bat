ccd73abdab485a89086b08697269fb0699219073
bigwindlee <bigwindlee@hotmail.com>
2016-04-12 00:57:39 +0800

    add jndi test into build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,11 @@
 @echo off
 
-@echo off
-
 IF /I "x%JAVA_HOME%" == "x" (
   ECHO JAVA_HOME is not set!
   GOTO :END
 )
 
-"%JAVA_HOME%\bin\javac" SSLProtocolTests.java
+"%JAVA_HOME%\bin\javac" SSLProtocolTests.java JndiTest.java
 
 
 :END
